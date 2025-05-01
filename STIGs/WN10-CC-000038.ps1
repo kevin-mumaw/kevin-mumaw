26834c92c995a5c48c1c75e9eae8ae8ad9a4b4e1
Kevin E. Mumaw <49378509+kevin-mumaw@users.noreply.github.com>
2025-05-01 19:20:38 -0400

    Add files via upload

---
+++ STIGs/WN10-CC-000038.ps1
@@ -0,0 +1,44 @@
+﻿<#
+.SYNOPSIS
+The PowerShell script modifies the registry to ensure that WDigest Authentication is disabled by setting the UseLogonCredential value to 0 in the specified registry path. This prevents the storage of plain text passwords in memory, thereby reducing the risk of credential theft.
+.NOTES
+    Author: Kevin Mumaw
+    LinkedIn: linkedin.com/in/kevin-mumaw-10/
+    GitHub: github.com/kevin-mumaw
+    Date Created: 2025-30-05
+    Last Modified: 2025-29-04
+    Version: 1.0
+    CVEs: N/A
+    Plugin IDs: N/A
+    STIG-ID: WN10-CC-000038
+
+.TESTED ON
+    Date(s) Tested: 
+    Tested By: 
+    Systems Tested: 
+    PowerShell Ver.: 5.1 Build 26100 Revision 3912
+
+.USAGE
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+# Define the registry path and value
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest"
+$valueName = "UseLogonCredential"
+$valueData = 0
+
+# Check if the registry path exists
+if (-Not (Test-Path $registryPath)) {
+    # Create the registry path if it does not exist
+    New-Item -Path $registryPath -Force
+}
+
+# Set the registry value to disable WDigest Authentication
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+# Output the result
+if ((Get-ItemProperty -Path $registryPath -Name $valueName).$valueName -eq $valueData) {
+    Write-Host "WDigest Authentication has been successfully disabled."
+} else {
+    Write-Host "Failed to disable WDigest Authentication."
+}
+
